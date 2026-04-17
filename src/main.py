fb8f8633da96752904a250a0b072875cf0b2c4f8
yadnexsh <yadnexshu@example.com>
2026-04-18 02:30:57 +0530

    #9 Cleaned up the code a lil , file related bugs are there , fix pending.

--- src/main.py
+++ src/main.py
@@ -17,7 +17,8 @@ from PySide2.QtWidgets import (
     QFileDialog,
     QSpacerItem,
     QButtonGroup,
-    QMessageBox
+    QMessageBox,
+    QFrame
 )
 from PySide2.QtGui import QPixmap, QStandardItemModel, QStandardItem, QIcon
 from PySide2.QtCore import Qt, Slot, QSize
@@ -31,20 +32,17 @@ import shutil
 CURRENT_FILEDIR = os.path.dirname(__file__)
 ROOT_FOLDER = os.path.dirname(CURRENT_FILEDIR)
 
-
 MEDIA_FOLDER = os.path.join(CURRENT_FILEDIR, "media")
 ICON_FOLDER = os.path.join(MEDIA_FOLDER, "icons")
 
-OUTPUT_FOLDER = os.path.join(ROOT_FOLDER, "output")
+NUKE_FOLDER = os.path.join((os.path.expanduser("~")), ".nuke")
+OUTPUT_FOLDER =  os.path.join(NUKE_FOLDER, "NodeVault_output")
 GIZMO_FOLDER = os.path.join(OUTPUT_FOLDER,"Gizmos")
-# TEMPLATE_FOLDER = os.path.join(OUTPUT_FOLDER,"Template")
-# SCRIPT_FOLDER = os.path.join(OUTPUT_FOLDER,"Scripts")
 
 USERNAME = os.getlogin()
 
-# THUMBNAIL_FILE = os.path.join(MEDIA_FOLDER, "heavily_compressed.png")
-IMAGE_ICON_PATH = os.path.join(ICON_FOLDER, "image_icon.png")
-VIDEO_ICON_PATH = os.path.join(ICON_FOLDER, "video_icon.png")
+ICON_IMAGE_PATH = os.path.join(ICON_FOLDER, "ICON_image.png")
+ICON_VIDEO_PATH = os.path.join(ICON_FOLDER, "ICON_video.png")
 
 FIXED_POLICY = QSizePolicy.Policy.Fixed
 FILETYPE_FOLDERS = ["Gizmos"]
@@ -96,15 +94,12 @@ class NodeVault_GUI(QWidget):
 
         self.init_library_ui()
         self.init_submit_ui()
-        self.init_detailed_ui()
-        
-
 
         # -------------- ASSEMBLE MAIN WINDOW --------------
         self.main_layout.addWidget(self.primary_tabs)
 
     def init_library_ui(self):
-
+        
         # -------------- LIBRARY TAB LAYOUT --------------
         self.library_master_layout = QHBoxLayout(self.library_tab)
 
@@ -116,19 +111,13 @@ class NodeVault_GUI(QWidget):
         model = QStandardItemModel()
         model.setHorizontalHeaderLabels(["Category"])
 
-        # self.template_item = QStandardItem("Templates")
         self.gizmos_item = QStandardItem("Gizmo")
-        # self.gizmos_item.
         self.deep_item = QStandardItem("Deep")
         self.image_item = QStandardItem("Image")
         self.draw_item = QStandardItem("Draw")
         self.time_item = QStandardItem("Time")
         self.channel_item = QStandardItem("Channel")
         self.filter_item = QStandardItem("Filter")
-        # self.tricks_item = QStandardItem("Tricks")
-        # self.misc_item = QStandardItem("Misc")
-
-        # model.appendRow(self.template_item)
 
         model.appendRow(self.gizmos_item)
         self.gizmos_item.appendRow(self.deep_item)
@@ -138,30 +127,24 @@ class NodeVault_GUI(QWidget):
         self.gizmos_item.appendRow(self.channel_item)
         self.gizmos_item.appendRow(self.filter_item)
 
-        # model.appendRow(self.tricks_item)
-        # model.appendRow(self.misc_item)
-
         self.category_panel.setModel(model)
         self.category_panel.expandAll()
         
         self.category_panel.clicked.connect(self.on_category_panel_clicked)
+        
         # -------------- ACTIVE FILTER BAR --------------
         self.tabs = QTabWidget()
 
-        self.all_tab = QWidget()
-        self.temp_tab = QWidget()
+        self.library_tab = QWidget()
 
-        self.tabs.addTab(self.all_tab, "All")
-        self.tabs.addTab(self.temp_tab, "Temp")
-        self.tabs.setTabsClosable(True)                 # how to stop ALL Tab getting closed ?
+
+        self.tabs.addTab(self.library_tab, "Library")
+        self.tabs.setTabsClosable(True)
         self.tabs.tabCloseRequested.connect(self.close_tabs)
-        self.tabs
-
 
         # -------------- GIZMO GRID (inside All filter) --------------
         self.files_grid_layout = QGridLayout()
-        self.all_tab.setLayout(self.files_grid_layout)
-
+        self.library_tab.setLayout(self.files_grid_layout)
 
         # -------------- CONTENT AREA LAYOUT  --------------
         self.right_panel_layout = QVBoxLayout()
@@ -170,81 +153,6 @@ class NodeVault_GUI(QWidget):
         # -------------- ASSEMBLE LIBRARY TAB --------------
         self.library_master_layout.addWidget(self.category_panel)
         self.library_master_layout.addLayout(self.right_panel_layout)
-        
-        
-        
-    def init_detailed_ui(self):
-        self.detailed_tab = QWidget()
-        self.tabs.addTab(self.detailed_tab, "Detailed Tab")
-        
-        self.detailed_tab_layout = QHBoxLayout()
-        
-        left_layout = QVBoxLayout()
-        lbox1 = QVBoxLayout()
-        
-        lbox2 = QVBoxLayout()
-        
-        
-        
-        right_layout = QVBoxLayout()
-        rbox1 = QVBoxLayout()
-        rbox2 = QVBoxLayout()
-        
-        
-        
-        filename = QLabel("Filename")
-        
-        filename_font = filename.font()
-        filename_font.setPointSize(14)
-        filename_font.setBold(True)
-        filename.setFont(filename_font)
-        
-        # test = QLabel(f"{filename} - {author}")
-        
-        author = QLabel("Author")
-        tagline = QLabel("Tagline")
-        node_info = QLabel("node_info")
-        node_info = QLabel("node_info")
-        submitted = QLabel("submitted")
-        resources = QLabel("resources")
-        repo = QLabel("repo")
-        issue = QLabel("issue")
-        docs = QLabel("docs")
-        desc = QLabel("desc")
-        
-        
-        
-
-        
-        sub = QPushButton("1")
-
-        
-        # left_layout.addWidget(test_btn1)
-        lbox1.addWidget(filename)
-        lbox1.addWidget(author)
-        lbox1.addWidget(tagline)
-        lbox2.addWidget(desc)
-        
-        rbox1.addWidget(node_info)
-        rbox1.addWidget(submitted)
-        rbox2.addWidget(resources)
-        rbox2.addWidget(repo)
-        rbox2.addWidget(issue)
-        rbox2.addWidget(docs)
-        
-        left_layout.addLayout(lbox1, 3)
-        left_layout.addLayout(lbox2, 7)
-        
-        right_layout.addLayout(rbox1)
-        right_layout.addLayout(rbox2)
-        
-        
-
-        
-        self.detailed_tab_layout.addLayout(left_layout, 7)          # 7 = 70% which is a stetch value in func.
-        self.detailed_tab_layout.addLayout(right_layout, 3)
-        self.detailed_tab.setLayout(self.detailed_tab_layout)
-        
         
         
     def init_submit_ui(self):
@@ -260,7 +168,6 @@ class NodeVault_GUI(QWidget):
         
         self.submit_master_layout.addWidget(self.submit_lbl)
         self.submit_master_layout.addSpacing(10)
-
 
         self.columns_layout = QHBoxLayout()
         self.main_left_box = QVBoxLayout()
@@ -295,14 +202,11 @@ class NodeVault_GUI(QWidget):
         self.filetype_bg.setExclusive(True)
         
         self.gizmo_btn = QPushButton("Gizmo")
-        # self.gizmo_btn.setEnabled(True)
-        # self.script_btn = QPushButton("Script")
-        # self.template_btn = QPushButton("Template")
-        
+
         # Making The buttons Clickable
         for btn in [self.gizmo_btn]:
             btn.setCheckable(True)
-            btn.setChecked(True)        # since we have 1file type  only use this
+            btn.setChecked(True)     
             self.filetype_bg.addButton(btn)
             self.filetype_box_layout.addWidget(btn)
 
@@ -320,7 +224,6 @@ class NodeVault_GUI(QWidget):
         desc_layout = QVBoxLayout(desc_group)
         self.desc_edit = QTextEdit()
         self.desc_edit.setPlaceholderText("Enter description…")
-        # self.desc_edit.setFixedHeight(80)
         desc_layout.addWidget(self.desc_edit)
 
         # --- Sub-Category Options  ---
@@ -419,8 +322,8 @@ class NodeVault_GUI(QWidget):
         # -------- Preview Images + Demo Video -----------
         self.media_box = QGroupBox("Preview Images & Demo Video")
         self.media_box_layout = QHBoxLayout(self.media_box)
-        self.image_icon = QIcon(IMAGE_ICON_PATH)
-        self.video_icon = QIcon(VIDEO_ICON_PATH)
+        self.image_icon = QIcon(ICON_IMAGE_PATH)
+        self.video_icon = QIcon(ICON_VIDEO_PATH)
 
         self.preview_btn_1 = QPushButton()
         self.preview_btn_2 = QPushButton()
@@ -458,9 +361,6 @@ class NodeVault_GUI(QWidget):
             self.media_box_layout.addWidget(each)
         self.media_box_layout.addWidget(self.demo_video_btn)
 
-
-        #---
-
         self.main_left_box.addWidget(self.information_box)
         self.main_left_box.addWidget(self.filetype_box)
 
@@ -471,17 +371,12 @@ class NodeVault_GUI(QWidget):
         
         self.main_left_box.addWidget(self.sub_category_box)
         self.main_left_box.addLayout(self.render_nuke_layout)
-        
-        # v_spacer_left = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)       # W H hdata policy and vdata policy
-        # self.main_left_box.addItem(v_spacer_left) 
 
 # ++++
         self.main_right_box.addWidget(self.docs_box)
         self.main_right_box.addWidget(self.external_box)
         self.main_right_box.addWidget(self.media_box)
-        
-        # v_spacer_right = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-        # self.main_right_box.addItem(v_spacer_right) 
+
         
         self.btn_submit = QPushButton("SUBMIT")
         self.btn_submit.setMinimumHeight(40) 
@@ -501,11 +396,6 @@ class NodeVault_GUI(QWidget):
         
         self.submit_master_layout.addWidget(self.btn_submit, alignment = Qt.AlignCenter)
         self.submit_master_layout.addStretch()
-
-
-        
-
-    
         
     # ***************** FUNC *******************
 
@@ -622,19 +512,14 @@ class NodeVault_GUI(QWidget):
             e = "Please enter a File Name."
             errors.append(e)
             
-
-        
-
-        submission_id = str(uuid.uuid4())
+        self.submission_id = str(uuid.uuid4())
         
         filetype = self.filetype_bg.checkedButton().text()
 
-            
         submitted_time = datetime.datetime.now().isoformat(timespec='seconds')
 
         filename = self.filename_le.text().strip()
         author = self.author_le.text()
-        
         version = 0 
 
         try:
@@ -668,7 +553,7 @@ class NodeVault_GUI(QWidget):
         extra_link = self.extra_link.text()
         
         data = {
-            "uuid": submission_id,
+            "uuid": self.submission_id,
             "submitted": submitted_time,
             "filetype": filetype,
             "filename": filename,
@@ -691,25 +576,23 @@ class NodeVault_GUI(QWidget):
         # Make Folder for Json File & Write JSON in it
         folder_map = {
             "Gizmo" : GIZMO_FOLDER,
-            # "Script" : SCRIPT_FOLDER,
-            # "Template" : TEMPLATE_FOLDER
         }
         
         try:
-            save_dir = os.path.join(folder_map[filetype], submission_id)
-            os.makedirs(save_dir, exist_ok=True)                        # GIZMO SCRIPTS TEMP folder creation
+            save_dir = os.path.join(folder_map[filetype], self.submission_id)
+            os.makedirs(save_dir, exist_ok=True) 
         except Exception as e:
             QMessageBox.critical(self, "Errors", f"Error > {e}")
 
             
-        json_filename = os.path.join(save_dir, f"{submission_id}.json")
+        json_filename = os.path.join(save_dir, f"{self.submission_id}.json")
         
         try:
             with open(json_filename, "w") as file:
                 json.dump(data, file, indent=4)
                 
             file_extension = os.path.splitext(self.main_file)[1]
-            new_filename = f"{submission_id}{file_extension}" 
+            new_filename = f"{self.submission_id}{file_extension}" 
             dst_path = os.path.join(save_dir, new_filename)
             shutil.copy(src=self.main_file, dst=dst_path)
             
@@ -771,22 +654,25 @@ class NodeVault_GUI(QWidget):
         folder_name = os.listdir(GIZMO_FOLDER)
         
         MAX_COLS = 10
-        
         counter = 0
+        
         clicked_text = index.data()
         print(clicked_text)
+        
         square_size = 85
+        
         while self.files_grid_layout.count():
             item = self.files_grid_layout.takeAt(0)
             item.widget().deleteLater()
         
         for each in folder_name:
-            filename = f"{each}.json"
-            each_folder = os.path.join(GIZMO_FOLDER, each)
-            each_json = os.path.join(each_folder, filename)
+            json_filename = f"{each}.json"
+            each_folder_path = os.path.join(GIZMO_FOLDER, each)
+            each_json = os.path.join(each_folder_path, json_filename)
             
             with open(each_json, "r") as file:
                 data = json.load(file)
+                submission_id = data["uuid"]
                 filename = data["filename"]
                 filetype = data["filetype"]
                 sub_category = data["sub_category"]
@@ -795,14 +681,107 @@ class NodeVault_GUI(QWidget):
                 col = counter % MAX_COLS
                 row = counter // MAX_COLS
                 each = QPushButton(filename)
+                each.setProperty("submission_id" , submission_id)
+                each.clicked.connect(self.on_gizmo_button_clicked)
                 each.setFixedSize(square_size, square_size)
                 self.files_grid_layout.addWidget(each, row, col)
-                counter += 1
                 
+                counter += 1
+
         if counter > 0:
             self.files_grid_layout.setColumnStretch(MAX_COLS, 1)
             self.files_grid_layout.setRowStretch(row + 1, 1)
-    
+            
+    def on_gizmo_button_clicked(self):
+        
+        button = self.sender()
+        submission_id = button.property("submission_id")
+        filetype_folder = os.path.join(GIZMO_FOLDER, submission_id)
+        json_path = os.path.join(filetype_folder, f"{submission_id}.json")
+            
+        with open(json_path, "r") as file:
+            data = json.load(file)
+            
+        # -----------------------------------------------
+        self.detailed_tab = QWidget()        
+        self.detailed_tab_layout = QHBoxLayout(self.detailed_tab)
+        
+        left_layout = QVBoxLayout()
+        
+        header_row = QHBoxLayout()
+        
+        self.det_filename = QLabel()
+        filename_font = self.det_filename.font()
+        filename_font.setBold(True)
+        filename_font.setPointSize(24)
+        self.det_filename.setFont(filename_font)
+        
+        self.det_author = QLabel()
+        
+        header_row.addWidget(self.det_filename)
+        header_row.addWidget(self.det_author)
+        header_row.addStretch()
+        
+        self.det_tagline = QLabel()
+        desc_label = QLabel()
+        self.det_desc = QTextEdit()
+        self.det_desc.setReadOnly(True)
+
+        left_layout.addLayout(header_row)
+        left_layout.addWidget(self.det_tagline)
+        left_layout.addWidget(desc_label)
+        left_layout.addWidget(self.det_desc)
+
+        right_layout = QVBoxLayout()
+
+        # -------- Node Info Group -------------
+        info_label = QLabel("Node Info")
+        info_form = QFormLayout()
+        info_form.addRow("Version", QLabel("v2.02.03"))
+        info_form.addRow("Submitted", QLabel("16 Feb 2026"))
+        info_form.addRow("Size", QLabel("23kb"))
+
+        # --------- Resources Group -------
+        res_label = QLabel("Resources")
+        self.res_repo = QPushButton("Repo")
+        self.res_issue = QPushButton("Issue")
+        self.res_docs = QPushButton("Docs")
+        self.subscribe = QPushButton("subscribe")
+        right_layout.addWidget(info_label)
+        right_layout.addLayout(info_form)
+        right_layout.addWidget(res_label)
+        right_layout.addWidget(self.res_repo)
+        right_layout.addWidget(self.res_issue)
+        right_layout.addWidget(self.res_docs)
+        right_layout.addWidget(self.subscribe)
+        right_layout.addStretch() 
+        self.detailed_tab_layout.addLayout(left_layout, 7)  
+        self.detailed_tab_layout.addLayout(right_layout, 3)
+        self.subscribe.setProperty("object_id", submission_id)
+        self.subscribe.clicked.connect(self.copy_object_file)
+        
+        
+        self.det_filename.setText(data["filename"])
+        self.det_author.setText(f"by {data['author']}")
+        self.det_tagline.setText(data['tagline']) 
+        self.det_desc.setPlainText(data["description"])
+        index = self.tabs.indexOf(self.detailed_tab)
+        index = self.tabs.addTab(self.detailed_tab, data["filename"])
+        self.tabs.setCurrentIndex(index)
+
+    def copy_object_file(self):
+        subscribe_btn = self.sender()
+        object_id = subscribe_btn.property("object_id")
+        filetype_folder = os.path.join(GIZMO_FOLDER, object_id)
+        print(filetype_folder)
+        for each in os.listdir(filetype_folder):
+            if each.endswith(".gizmo"):
+                file = os.path.join(filetype_folder, each)
+
+
+        
+        shutil.copy(src=file, dst=nodevault_folder)
+        
     def close_tabs(self, index):
         if index == 0:
             QMessageBox.critical(self, "Error", "You cant close ALL Tab")
@@ -813,6 +792,7 @@ class NodeVault_GUI(QWidget):
         self.save_json()
 
 window =  None
+
 def launch():
     global window
     window = NodeVault_GUI()
